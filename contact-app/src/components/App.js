8c3e86a989d8c70f3056b4b3d92c4d7bdc41efcf
Yuki Nakata <nakata.yuki@plus-zero.co.jp>
2021-08-14 16:31:31 +0900

    連絡先情報をブラウザのローカルストレージに保存する処理の実装

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact]);
+  };
+
+  useEffect(() => {
+    const retriverContacts = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+    );
+    if (retriverContacts) setContacts(retriverContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <div>
       <Header></Header>
-      <AddContact></AddContact>
+      <AddContact addContactHandler={addContactHandler}></AddContact>
       <ContactList contacts={contacts}></ContactList>
     </div>
   );
